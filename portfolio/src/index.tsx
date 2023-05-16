161127182cd8e4186ff4eab9d7bddd13ecd8f819
nivek724 <kevin.nivek.choe@gmail.com>
2023-05-15 23:18:46 -0700

    fix window position on page switch

--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ScrollToTop from 'Components/ScrollToTop';
 import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -14,6 +15,7 @@ root.render(
         href='https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
         rel='stylesheet'
       />
+      <ScrollToTop />
       <App />
     </React.StrictMode>
   </Router>
